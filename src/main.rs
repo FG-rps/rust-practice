3d29ccb9e160df6cce1558553889a4505713d116
Your Name <you@example.com>
2025-06-03 14:32:57 +0530

    Finiahed Fibonacci program

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,46 @@
-use core::f64;
-use std::{
-    io::{self, Read},
-    result,
-};
+use std::{io, process::Termination};
 
 fn main() {
-    let mut temprature_in_farenheit = String::new();
-
+    //Ask the user for term_number to calciulate
+    let mut term_number = String::new();
     io::stdin()
-        .read_line(&mut temprature_in_farenheit)
-        .expect("Failed to read user input");
+        .read_line(&mut term_number)
+        .expect("Failed to read input");
 
-    let temprature_in_farenheit: f64 = temprature_in_farenheit
+    let term_number: i128 = term_number
         .trim()
-        .parse::<f64>()
-        .expect("Failed to parse as 64 bit float");
+        .parse::<i128>()
+        .expect("Not valid number");
 
-    println!("{}", convert_to_celcius(temprature_in_farenheit));
+    //Calculate and print the value of nth_term of
+    println!(
+        "The {} th term of faboncci sequence is {}",
+        term_number,
+        nth_term(term_number)
+    );
 }
 
-fn convert_to_celcius(farenheit: f64) -> f64 {
-    let celcius: f64 = (farenheit / 1.8) - 32.0;
-    celcius
+fn nth_term(term_mumber: i128) -> i128 {
+    //Sign of the term_number
+    let sign: i128 = term_mumber / term_mumber.abs();
+
+    let mut first_term: i128 = 0;
+    let mut second_term: i128 = 1 * sign;
+    let mut third_term: i128 = 0;
+
+    let mut counter: u64 = 0;
+    let term_counter: u64 = term_mumber.abs() as u64; // For comparing against counter
+
+    let nth_term: i128 = loop {
+        while counter < term_counter {
+            third_term = first_term + second_term;
+            first_term = second_term;
+            second_term = third_term;
+            counter += 1;
+        }
+        if counter == term_counter {
+            break third_term;
+        }
+    };
+    nth_term
 }
